94e34ea22a08dff32c5fc95cacf83564981e58b4
Tony <torretony94@gmail.com>
2023-05-05 20:43:50 +0200

    aggiunto tutto

--- nome-progetto-di-esempio/routes/web.php
+++ nome-progetto-di-esempio/routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/stringa', function () {
+    return 'sono una stringa';
+});
+
+Route::get('/intero', function () {
+    return 323232;
+});
+
+
+Route::get('/array', function () {
+    $array = ['sono','un','array'];
+    return $array;
+});
+
+Route::get('/chisiamo', function () {
+    return view('chisiamo');
+});
+
+Route::get('/contatti', function () {
+    return view('contatti');
+});
+
+Route::get('/storia', function () {
+    return view('storia');
+});
